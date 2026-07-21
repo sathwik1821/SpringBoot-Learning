58d2cf939c43001897cc1cd092274b5b44140603
sathwik1821 <sathwikbodakunta2005@gmail.com>
2026-07-21 10:46:33 +0530

    Introduced pagination

--- src/main/java/com/codingshuttle/sathwik/jpaTutorial/controllers/ProductController.java
+++ src/main/java/com/codingshuttle/sathwik/jpaTutorial/controllers/ProductController.java
@@ -3,6 +3,8 @@ package com.codingshuttle.sathwik.jpaTutorial.controllers;
 
 import com.codingshuttle.sathwik.jpaTutorial.entites.ProductEntity;
 import com.codingshuttle.sathwik.jpaTutorial.repositories.ProductRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,13 +16,20 @@ public class ProductController {
 
     final ProductRepository productRepository;
 
+    final int PAGE_SIZE=5;
+
     public ProductController(ProductRepository productRepository) {
         this.productRepository=productRepository;
     }
 
+
+
     @GetMapping(path = "/getAll")
-    public List<ProductEntity> getAll(@RequestParam(defaultValue = "id") String sortBy) {
-        return productRepository.findAll(Sort.by(sortBy));
+    public List<ProductEntity> getAll(@RequestParam(defaultValue = "1") String sortBy,
+                                      @RequestParam(defaultValue = "1") Integer pageNumber) {
+
+        Pageable pageable = PageRequest.of(pageNumber, PAGE_SIZE, Sort.by(sortBy).ascending());
+        return productRepository.findAll(pageable).getContent();
     }
 
     @GetMapping(path = "/{productName}")
