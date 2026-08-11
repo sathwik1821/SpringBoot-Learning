2ba807157ddfa96624476681c51e258e674f7931
sathwik1821 <sathwikbodakunta2005@gmail.com>
2026-08-11 15:59:03 +0530

    added logging

--- src/main/java/com/codingshuttle/sathwik/prod_ready_features/clients/impls/EmployeeClientImpl.java
+++ src/main/java/com/codingshuttle/sathwik/prod_ready_features/clients/impls/EmployeeClientImpl.java
@@ -5,6 +5,8 @@ import com.codingshuttle.sathwik.prod_ready_features.clients.EmployeeClient;
 import com.codingshuttle.sathwik.prod_ready_features.dto.EmployeeDTO;
 import com.codingshuttle.sathwik.prod_ready_features.exceptions.RescourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
@@ -17,12 +19,18 @@ import java.util.List;
 @Service
 public class EmployeeClientImpl implements EmployeeClient {
 
+    Logger logger = LoggerFactory.getLogger(EmployeeClientImpl.class);
+
     private final RestClient restClient;
 
 
     @Override
     public List<EmployeeDTO> getAllEmployees() {
         try{
+            logger.info("Get all employees started");
+            logger.warn("Get all employees started so you might get Database error");
+            logger.debug("Get all employees started so you might get Database error DEBUG");
+            logger.trace("Get all employees started so you might get Database error TRACE");
             ApiResponse<List<EmployeeDTO>> employeeDTOList =restClient.get()
                     .uri("employees")
                     .retrieve()
@@ -32,6 +40,9 @@ public class EmployeeClientImpl implements EmployeeClient {
                     .body(new ParameterizedTypeReference<ApiResponse<List<EmployeeDTO>>>() {
                     });
             assert employeeDTOList != null;
+            System.out.println(employeeDTOList.getData());
+            logger.info("Get all employees completed");
+
             return employeeDTOList.getData();
         }
         catch (Exception e){
