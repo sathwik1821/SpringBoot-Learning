82a82799b5bce92e39ce24970ee0f32a641ca735
sathwik1821 <sathwikbodakunta2005@gmail.com>
2026-08-16 18:04:04 +0530

    added jwt

--- src/main/java/com/codingshuttle/sathwik/SecurityApplication/entities/User.java
+++ src/main/java/com/codingshuttle/sathwik/SecurityApplication/entities/User.java
@@ -1,7 +1,9 @@
 package com.codingshuttle.sathwik.SecurityApplication.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +15,8 @@ import java.util.List;
 @Entity
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User implements UserDetails {
 
 
---
+++ src/test/java/com/codingshuttle/sathwik/SecurityApplication/JwtServiceTests.java
@@ -0,0 +1,26 @@
+package com.codingshuttle.sathwik.SecurityApplication;
+
+import com.codingshuttle.sathwik.SecurityApplication.entities.User;
+import com.codingshuttle.sathwik.SecurityApplication.services.JwtService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JwtServiceTests {
+
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Test
+    void getUserDetails() {
+        User user=new User(18L,"sathwik@gmail.com","sathwik@18");
+
+        String token=jwtService.generateToken(user);
+        System.out.println(token);
+
+        Long id=jwtService.getUserIdFromToken(token);
+        System.out.println(id);
+    }
+}
