590f70b7d74eb2b1f2645c09bd3bbfc2f0b50f43
sathwik1821 <sathwikbodakunta2005@gmail.com>
2026-08-20 13:30:30 +0530

    Added  Security method annotations

--- src/main/java/com/codingshuttle/sathwik/SecurityApplication/configs/WebSecurityConfig.java
+++ src/main/java/com/codingshuttle/sathwik/SecurityApplication/configs/WebSecurityConfig.java
@@ -5,27 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authorization.AuthorityAuthorizationManager;
-import org.springframework.security.authorization.AuthorizationManagers;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-
-import org.springframework.http.HttpMethod;
-
 import jakarta.servlet.http.HttpServletResponse;
-
-import static com.codingshuttle.sathwik.SecurityApplication.dto.Permission.*;
-import static com.codingshuttle.sathwik.SecurityApplication.dto.enums.Role.ADMIN;
-import static com.codingshuttle.sathwik.SecurityApplication.dto.enums.Role.USER;
 
 @Configuration
 @EnableWebSecurity
@@ -43,20 +31,9 @@ public class WebSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeHttpRequests(auth -> auth
-                    .requestMatchers(publicRoutes).permitAll()
-                    .requestMatchers(HttpMethod.GET,"/posts", "/posts/**")
-                        .hasAuthority(POST_VIEW.name())
-                    .requestMatchers(HttpMethod.POST, "/posts", "/posts/**")
-                            .access(AuthorizationManagers.allOf(
-                                    AuthorityAuthorizationManager.hasAnyRole(
-                                            ADMIN.name(),
-                                            USER.name()
-                                    ),
-                                    AuthorityAuthorizationManager.hasAuthority(POST_CREATE.name()),
-                                    AuthorityAuthorizationManager.hasAuthority(POST_VIEW.name())
-                            ))
-
-                .anyRequest().authenticated())
+                        .requestMatchers(publicRoutes).permitAll()
+                        .requestMatchers("/posts/**").authenticated()
+                        .anyRequest().authenticated())
                 .csrf(csrfConfig -> csrfConfig.disable())
                 .sessionManagement(sessionConfig -> sessionConfig
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
--- src/main/java/com/codingshuttle/sathwik/SecurityApplication/controllers/PostController.java
+++ src/main/java/com/codingshuttle/sathwik/SecurityApplication/controllers/PostController.java
@@ -4,6 +4,8 @@ import com.codingshuttle.sathwik.SecurityApplication.dto.PostDTO;
 import com.codingshuttle.sathwik.SecurityApplication.services.PostService;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,19 +15,22 @@ import java.util.List;
 @RequestMapping(path = "/posts" )
 public class PostController {
 
-    private  final PostService postService;
+    private final PostService postService;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('POST_VIEW')")
     public List<PostDTO> findAllPosts() {
         return postService.findAllPosts();
     }
 
     @GetMapping(path = "/{postId}")
+    @PreAuthorize("hasAuthority('POST_VIEW')")
     public PostDTO findPostById(@PathVariable Long postId) {
         return postService.findPostById(postId);
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('POST_CREATE') and hasAnyRole('ADMIN', 'USER')")
     public PostDTO createNewPost(@RequestBody PostDTO postDTO) {
         return postService.createNewPost(postDTO);
     }
